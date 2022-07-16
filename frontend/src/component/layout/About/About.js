b20d5aa5609c585f5165f9714794c70105de6631
noman-sajid <noman15072002@gmail.com>
2022-07-16 17:24:32 +0500

    first commit

---
+++ frontend/src/component/layout/About/About.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./aboutSection.css";
+import { Button, Typography, Avatar } from "@material-ui/core";
+import YouTubeIcon from "@material-ui/icons/YouTube";
+import InstagramIcon from "@material-ui/icons/Instagram";
+const About = () => {
+  const visitInstagram = () => {
+    window.location = "https://instagram.com/noman_sajid01";
+  };
+  return (
+    <div className="aboutSection">
+      <div></div>
+      <div className="aboutSectionGradient"></div>
+      <div className="aboutSectionContainer">
+        <Typography component="h1">About Us</Typography>
+
+        <div>
+          <div>
+            <Avatar
+              style={{ width: "10vmax", height: "10vmax", margin: "2vmax 0" }}
+              src="https://res.cloudinary.com/dccxxqwnt/image/upload/v1657888465/local/20160410_073514-1_df8t5g.jpg"
+              alt="Founder"
+            />
+            <Typography>Noman Sajid</Typography>
+            <Button onClick={visitInstagram} color="primary">
+              Visit Instagram
+            </Button>
+            <span>
+              This is a sample ecomerse websit made for portfolio project
+            </span>
+          </div>
+          <div className="aboutSectionContainer2">
+            <Typography component="h2">Our Brands</Typography>
+          
+
+            <a href="https://instagram.com/noman_sajid01" target="blank">
+              <InstagramIcon className="instagramSvgIcon" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
